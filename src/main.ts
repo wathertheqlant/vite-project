afc8b4b2482536dc5f25502e1d9cd1d1de913b17
kyoungmin.kim <kkgg7527@gmail.com>
2025-04-16 16:42:46 +0900

    fix: remove nouse import

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import "./style.css";
 import "./index.css";
-import typescriptLogo from "./typescript.svg";
-import viteLogo from "/vite.svg";
+
 import { setupCounter } from "./counter.ts";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-  <h1 class="text-3xl font-bold">Hello</h1>
+    <h1 class="text-3xl font-bold">Hello</h1>
   </div>
 `;
 
